55a259f2530a25c730cdc30c3197f9c170cfcb34
Christopher Couch <c.alan.couch@gmail.com>
2020-10-26 15:07:53 -0500

    Created the CustomList<T> generic class, gave it the variables of count, capacity, and the array T[].

---
+++ CustomListUnitTests/CustomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListUnitTests
+{
+    public class CustomList<T>
+    {
+        int count;
+        int capacity;
+        T[] items;
+
+        public void Add(T items)
+        {
+            //logic to add items to the array.
+            
+        }
+    }
+}
